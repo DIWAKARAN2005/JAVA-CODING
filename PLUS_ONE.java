60cf7bc366735dfc4c3d55a152c491b7e73c5760
DIWAKARAN2005 <148414008+DIWAKARAN2005@users.noreply.github.com>
2026-06-18 15:25:35 +0530

    Create PLUS_ONE.java

---
+++ PLUS_ONE.java
@@ -0,0 +1,14 @@
+digits = [1,2,3]
+
+for i in range(len(digits)-1, -1, -1):
+
+    if digits[i] < 9:
+        digits[i] = digits[i] + 1
+        break
+
+    digits[i] = 0
+
+else:
+    digits = [1] + digits
+
+print(digits)
